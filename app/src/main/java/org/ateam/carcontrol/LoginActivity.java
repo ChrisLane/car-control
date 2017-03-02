175d6c9727f99d81d83a5485c9ed82c284e36d6e
Chris Lane <christopher_lane@live.co.uk>
2017-03-02 17:04:03 +0000

    Start main menu after login

--- app/src/main/java/org/ateam/carcontrol/LoginActivity.java
+++ app/src/main/java/org/ateam/carcontrol/LoginActivity.java
@@ -3,6 +3,7 @@ package org.ateam.carcontrol;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -333,7 +334,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                Intent launchMain = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(launchMain);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
